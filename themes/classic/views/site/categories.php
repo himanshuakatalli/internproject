7f3fea7250787fe632592c4a232b2e0eb03a0920
nitesh-kumar-gupta <nitesh.gupta@venturepact.com>
2016-03-29 12:35:36 +0530

    catagory page solved

--- themes/classic/views/site/categories.php
+++ themes/classic/views/site/categories.php
@@ -1,56 +1,65 @@
 <div style="background: #f2f2f2; width: 100%; padding: 2em 0;">
-<section class="categories">
-    <hgroup>
-      <h2><b>Browse</b> our software categories</h2>
-      <h4>Find your software in one of our 300+ categories - from Accounting to Yoga, we cover it all!</h4>
-    </hgroup>
-    <main class="content">
-      <div class="cat-list">
-        <form>
-          <input type="text" id="cat-search" placeholder="Search in categories"></input>
-          <div class="cat-view">
-            <?php $categories=Categories::model()->findAllByAttributes(array('status'=>1)); 
-            $flag = true;
-            $prev = $categories[0]->name[0];
-            foreach($categories as $category)
-            {
-              $curr = $category->name[0]; 
-              if(ctype_digit($curr))
-              {
-              ?>
-                <div class='cat-view'>
-                <div class='cat-row'><h2>#</h2><ul><a href='#'><li><?php echo $category->name?></li></a>
-                </ul></div>
-              <?php 
-              }
-              else
-              {
-                x:if($flag)
-                  {
-                    ?>
-                    <div class='cat-view'><div class='cat-row'><h2><?php echo $curr?></h2><ul>
-                    <a href='#'><li><?php echo $category->name?></li></a>
-                    <?php
-                    $prev = $curr;
-                    $flag = false;
-                  }
-                  elseif($prev == $curr)
-                  {
-                    ?>
-                    <a href='#'><li><?php echo $category->name?></li></a>
-                    <?php
-                  }
-                  else
-                  {
-                    ?>
-                    </ul></div>
-                    <?php
-                    $prev = $curr;
-                    $flag = true;
-                    goto x;
-                  }
-              }
-          } ?>
+    <section class="categories">
+        <hgroup>
+          <h2><b>Browse</b> our software categories</h2>
+          <h4>Find your software in one of our 300+ categories - from Accounting to Yoga, we cover it all!</h4>
+        </hgroup>
+        <main class="content">
+            <div class="cat-list">
+                <form>
+                    <input type="text" id="cat-search" placeholder="Search in categories"/>
+                    <div class='cat-view'>
+                        <?php
+                        $categories=Categories::model()->findAllByAttributes(array('status'=>1));
+                        $flag = true;
+                        $prev = $categories[0]->name[0];
+                        foreach($categories as $category){
+                            $curr = $category->name[0];
+                            if(ctype_digit($curr)){
+                        ?>
+                            <div class='cat-row'>
+                                <h2>#</h2>
+                                <ul>
+                                    <a href='#'>
+                                        <li><?php echo $category->name?></li>
+                                    </a>
+                                </ul>
+                            </div>
+                        <?php
+                            }
+                            else{
+                                x:if($flag){
+                        ?>
+                            <div class='cat-row'>
+                                <h2><?php echo $curr?></h2>
+                                <ul>
+                                    <a href='#'>
+                                        <li><?php echo $category->name?></li>
+                                    </a>
+                        <?php
+                                $prev = $curr;
+                                $flag = false;
+                            }
+                            elseif($prev == $curr){
+                        ?>
+                                    <a href='#'>
+                                        <li><?php echo $category->name?></li>
+                                    </a>
+                        <?php
+                            }
+                            else{
+                        ?>
+                                </ul>
+                            </div>
+                        <?php
+                                $prev = $curr;
+                                $flag = true;
+                                goto x;
+                            }
+                        }
+                    } ?>
+                  </ul>
+                </div>
           </div>
         </form>
       </div>
