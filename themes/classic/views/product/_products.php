aedad134c846ae981242d25b1070a40dc80f21bf
Vikash Dahiya <vikash.dahiya@venturepact.com>
2016-04-01 12:50:56 +0530

    link according to ppc

--- themes/classic/views/product/_products.php
+++ themes/classic/views/product/_products.php
@@ -5,23 +5,42 @@
       <div class="well w3-card-12 w3-animate-opacity">
           <div class="media">
               <div class="media-left media-middle">
-                  <a href=<?php echo '"'.$product->product_website.'"' ?>>
+                  <a href="<?php echo Yii::app()->createUrl('/product/productprofile/',array('id'=>$product->id))?>">
                        <img class="media-object img-thumbnail" src=<?php echo Yii::app()->request->baseUrl.'/themes/product_logo/'.$product->logo.'.png'; ?> alt="Pic">
                   </a>
               </div>
               <div class="media-body">
                   <div class="col-sm-12 col-md-8 ">
-                      <a href=<?php echo '"'.$product->product_website.'"' ?>>
-                          <h3> <?php echo $product->name ?></h3>
-                      </a>
+                    <h2>
+                        <?php 
+                            if($product->under_ppc==0)
+                            {
+                              echo CHtml::link($product->name,array('controller/action'),array('class' => ''));
+                            }
+                            else
+                            {
+                              echo CHtml::link($product->name,$product->product_website,array('class' => ''));
+                            }
+                         ?>
+                    </h2>     
                       <h4>
                           <small>
                                by <?php echo $product->company_name ?>
                           </small>
                       </h4>
-                      <div class="rating" data-rating-max="5">
-
-                      </div>
+                      
+                  </div>
+                  <div>
+                      <?php 
+                          if($product->under_ppc==0)
+                          {
+                            echo CHtml::link('Vist Profile',array('controller/action'),array('class' => 'btn btn-primary','type'=>'button'));
+                          }
+                          else
+                          {
+                            echo CHtml::link('Vist Website',$product->product_website,array('class' => 'btn btn-primary','type'=>'button'));
+                          }
+                       ?>
                   </div>
                   <div class="col-sm-12">
 
