355fafdae31917e703e42a6019f3454f89181988
Abhishek <abhishek.singh@venturepact.com>
2016-04-01 10:04:06 +0530

    removed unneccessary google tag manager, which was taking longer time to load

--- themes/classic/views/site/reset.php
+++ themes/classic/views/site/reset.php
@@ -1,7 +1,6 @@
 <style>
-input{}
+#input{padding:20px;}
 </style>
-
 
 <div class="container" style="margin-top:200px;height:400px">
 
@@ -10,7 +9,7 @@ input{}
 		<h3 class="fs26 font_newlight team-text-blue mb20">Set Your Password</h3>
 		<div class="well" style="padding:30px">
 			<?php $form=$this->beginWidget('CActiveForm', array('id'=>'reset-form','enableClientValidation'=>true,'clientOptions'=>array('validateOnSubmit'=>true),'htmlOptions'=>array('class'=>"panel-default",'data-parsley-validate'=>'data-parsley-validate'))); ?>
-			<?php echo $form->passwordField($reset,'Password',array('class'=>'form-control','data-parsley-required-message'=>'Password is required','placeholder'=>"Password",'data-parsley-minlength'=>"6",'required'=>'required','id'=>'passwordField','data-parsley-required-message'=>'Password is required')); ?>
+			<?php echo $form->passwordField($reset,'Password',array('id'=>'input','class'=>'form-control','data-parsley-required-message'=>'Password is required','placeholder'=>"Password",'data-parsley-minlength'=>"6",'required'=>'required','id'=>'passwordField','data-parsley-required-message'=>'Password is required')); ?>
 			<br>
 			<?php  echo $form->passwordField($reset,'Confirm_Password',array('class'=>'form-control','data-parsley-required-message'=>'Confirm Password is required','placeholder'=>"Confirm Password",'data-parsley-minlength'=>"6",'required'=>'required','id'=>'passwordField','data-parsley-required-message'=>'Confirm Password is required','data-parsley-equalto'=>"#passwordField")); ?>
 
